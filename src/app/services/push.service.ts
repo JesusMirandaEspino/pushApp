6a613c29ef225a901e20f26f98a4ca0624782b43
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-06-02 21:53:34 -0500

    oneSignal

---
+++ src/app/services/push.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { OneSignal } from '@awesome-cordova-plugins/onesignal/ngx';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PushService {
+
+  constructor( private oneSignal: OneSignal ) { }
+
+
+  configuracionInicial(){
+
+    this.oneSignal.startInit('80829c2f-85f8-4c35-a167-1152d0b2c77c', '900385423612');
+
+    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+
+    this.oneSignal.handleNotificationReceived().subscribe((noti) => {
+    // do something when notification is received
+      console.log('recibe', noti );
+    });
+
+    this.oneSignal.handleNotificationOpened().subscribe((noti) => {
+      // do something when a notification is opened
+      console.log('open', noti );
+    });
+
+    this.oneSignal.endInit();
+
+  }
+
+
+}
